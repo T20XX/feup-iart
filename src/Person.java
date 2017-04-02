134d3f5fe8f0f5831648c49aabdff7b7c400c512
Telmo Barros <telmo.20@hotmail.com>
2017-04-02 23:57:40 +0100

    relatorio intercalar

--- src/Person.java
+++ src/Person.java
@@ -1,40 +1,27 @@
 
 public class Person {
-	
+
+	private String name;
 	private int age;
 	private Profession profession;
 	private Hobby[] hobbies;
 	private Group group;
-	
-	public Person(int age, Profession profession, Hobby[] hobbies){
+
+	public Person(String name, int age, Profession profession, Hobby[] hobbies){
+		this.setName(name);
 		this.age = age;
 		this.profession = profession;
 		this.hobbies = hobbies;
-	}
-	
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
-	public Profession getProfession() {
-		return profession;
-	}
-	public void setProfession(Profession profession) {
-		this.profession = profession;
-	}
-	public Hobby[] getHobbies() {
-		return hobbies;
-	}
-	public void setHobbies(Hobby[] hobbies) {
-		this.hobbies = hobbies;
-	}
-	public Group getGroup() {
-		return group;
-	}
-	public void setGroup(Group group) {
-		this.group = group;
 	}
 
+	public String getName() { return name; }
+	public void setName(String name) { this.name = name; }
+	public int getAge() { return age; }
+	public void setAge(int age) { this.age = age; }
+	public Profession getProfession() { return profession; }
+	public void setProfession(Profession profession) { this.profession = profession; }
+	public Hobby[] getHobbies() { return hobbies; }
+	public void setHobbies(Hobby[] hobbies) { this.hobbies = hobbies; }
+	public Group getGroup() { return group; }
+	public void setGroup(Group group) { this.group = group; }
 }
